61e1ad820df064a968137746154adf6fd0b09237
Raphael <Y962870451@126.com>
2021-02-26 21:10:20 +0800

    Initial commit

---
+++ app/src/main/java/com/yue/statusbarlyric/MainActivity.java
@@ -0,0 +1,131 @@
+package com.yue.statusbarlyric;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+//x，y坐标
+    private TextView xlabel;
+    private TextView ylabel;
+//    权限相关
+    private static final String ACTION_NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+    public boolean isNotificationListenerEnabled(Context context) {
+        Set<String> packageNames = NotificationManagerCompat.getEnabledListenerPackages(this);
+        return packageNames.contains(context.getPackageName());
+    }
+    public void openNotificationAccess() {
+        startActivity(new Intent(ACTION_NOTIFICATION_LISTENER_SETTINGS));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        xlabel = findViewById(R.id.xedit);
+        ylabel = findViewById(R.id.yedit);
+
+        if (!load()) {
+            ////////使用AlertDialog（弹出窗口信息），创建对象
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+            ////////设置标题
+            alert.setTitle("提示信息！");
+            alert.setIcon(R.drawable.ic_launcher_foreground);
+            alert.setMessage("本应用需要使用通知权限，但不需要联网权限，不放心可以将网络权限关闭。\n本应用目前只适配网易云音乐。\n使用本应用需要将应用保留在后台。" +
+                    "\n使用状态栏歌词只需点击“开启歌词悬浮窗”即可。\n使用前需要先点击两次“开启歌词悬浮窗按钮”，开启相应权限。\n" +
+                    "未经允许不可传播本应用！！！\n未经允许不可传播本应用！！！\n未经允许不可传播本应用！！！\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t——————————龠");
+            alert.setButton(AlertDialog.BUTTON_POSITIVE, "已阅", (dialog, which) -> { });
+            alert.setCancelable(true);
+            save();
+            alert.show();
+        }
+    }
+
+    public void stopFloatService(View view) {
+        if (NotificationTask.isStarted) {
+            Intent intent = new Intent("yueServicelyrics");
+            intent.putExtra("judgelyric",false);
+            sendBroadcast(intent);
+            startService(new Intent(MainActivity.this,NotificationTask.class));
+        }
+        Toast.makeText(MainActivity.this, "已关闭FloatWindow", Toast.LENGTH_SHORT).show();
+    }
+
+//授权，开启悬浮窗
+    public void startFloatingService(View view) {
+        if (NotificationTask.isStarted) {
+            this.stopFloatService(view);
+            Intent intent = new Intent("yueServicelyrics");
+            intent.putExtra("judgelyric",true);
+            intent.putExtra("x",Integer.parseInt(xlabel.getText().toString()));
+            intent.putExtra("y",Integer.parseInt(ylabel.getText().toString()));
+            sendBroadcast(intent);
+            startService(new Intent(MainActivity.this,NotificationTask.class));
+        } else {
+            if (isNotificationListenerEnabled(this)) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Toast.makeText(this,"当前无权限，请授权",Toast.LENGTH_SHORT).show();
+                    startActivityForResult(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:"+getPackageName())), 0);
+                } else {
+                    Intent intent = new Intent("yueServicelyrics");
+                    intent.putExtra("judgelyric",true);
+                    intent.putExtra("x",Integer.parseInt(xlabel.getText().toString()));
+                    intent.putExtra("y",Integer.parseInt(ylabel.getText().toString()));
+                    sendBroadcast(intent);
+                    startService(new Intent(MainActivity.this,NotificationTask.class));
+                }
+            } else {
+                openNotificationAccess();
+            }
+        }
+        Toast.makeText(MainActivity.this, "已开启FloatWindow", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "授权失败", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "授权成功", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void helpinfo(View v) {
+        ///////使用AlertDialog（弹出窗口信息），创建对象
+        AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+        ////////设置标题
+        alert.setTitle("提示信息！");
+        alert.setIcon(R.drawable.ic_launcher_foreground);
+        alert.setMessage("本应用需要使用通知权限，但不需要联网权限，不放心可以将网络权限关闭。\n本应用目前只适配网易云音乐。\n使用本应用需要将应用保留在后台。" +
+                "\n使用状态栏歌词只需点击“开启歌词悬浮窗”即可。\n使用前需要先点击两次“开启歌词悬浮窗按钮”，开启相应权限。\n" +
+                "未经允许不可传播本应用！！！\n未经允许不可传播本应用！！！\n未经允许不可传播本应用！！！\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t——————————龠");
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, "已阅", (dialog, which) -> { });
+        alert.setCancelable(true);
+        alert.show();
+    }
+    private void save() {
+        @SuppressLint("WrongConstant")
+        SharedPreferences editor = getSharedPreferences("data", Context.MODE_PRIVATE);
+        editor.edit().putBoolean("judge", true).apply();
+    }
+    private boolean load() {
+        SharedPreferences editor = getSharedPreferences("data",Context.MODE_PRIVATE);
+        return editor.getBoolean("judge",false);
+    }
+}
---
+++ app/src/main/java/com/yue/statusbarlyric/NotificationTask.java
@@ -0,0 +1,190 @@
+package com.yue.statusbarlyric;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Color;
+import android.graphics.PixelFormat;
+import android.os.Build;
+import android.provider.Settings;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.support.v4.media.MediaMetadataCompat;
+import android.support.v4.media.session.MediaControllerCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.media.app.NotificationCompat;
+
+
+import static android.support.v4.media.MediaMetadataCompat.METADATA_KEY_TITLE;
+
+public class NotificationTask  extends NotificationListenerService {
+    public String str = "";
+//歌词坐标，判断歌词是否显示出来
+    private int xlabel;
+    private int ylabel;
+    private boolean judgelyric = false;
+//    接收上述几个参数的receiver
+    private receiverlyrics receiverlyrics;
+    private TextView txt;
+    public static boolean isStarted = false;
+//悬浮窗
+    private WindowManager windowManager;
+    private WindowManager.LayoutParams layoutParams;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onCreate() {
+        txt = new TextView(getApplicationContext());
+        txt.setText("Nothing");
+        //接收停止接收歌词信息通知广播
+        receiverlyrics = new receiverlyrics();
+        IntentFilter intentFilter1 = new IntentFilter();
+        intentFilter1.addAction("yueServicelyrics");
+        registerReceiver(receiverlyrics,intentFilter1);
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy() {
+        this.unregisterReceiver(receiverlyrics);
+        System.out.println("Destroy");
+        stopSelf();
+        super.onDestroy();
+    }
+
+    private final MediaControllerCompat.Callback mMediaControllerCallback = new MediaControllerCompat.Callback() {
+        @Override
+        public void onMetadataChanged(MediaMetadataCompat metadata) {
+            //获取歌词信息
+            str = metadata.getString(METADATA_KEY_TITLE);
+            txt.setText(str);
+            super.onMetadataChanged(metadata);
+        }
+    };
+
+    @SuppressLint("RtlHardcoded")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (judgelyric) {
+            isStarted = true;
+//            以悬浮窗形式
+            windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+            layoutParams = new WindowManager.LayoutParams();
+            //根据安卓版本进行悬浮窗选择
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                layoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+            } else {
+                layoutParams.type = WindowManager.LayoutParams.TYPE_PHONE;
+            }
+            //透明
+            layoutParams.format = PixelFormat.RGBA_8888;
+            layoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+            layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN ;
+            //初始位置
+            layoutParams.width = 1000;
+            layoutParams.height = 100;
+            layoutParams.x = xlabel;
+            layoutParams.y = ylabel;
+            System.out.println("Start");
+            showFloatWindow();
+            Toast.makeText(NotificationTask.this, "Start", Toast.LENGTH_SHORT).show();
+        } else {
+            isStarted = false;
+            System.out.println("Stop");
+            closeFloatWindow();
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    //通知出现变化
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        if (judgelyric) {
+            System.out.println("Posted");
+            for (StatusBarNotification mn : getActiveNotifications()) {
+                if (mn.getPackageName() != null) {
+                    if (mn.getPackageName().equals("com.netease.cloudmusic")) {
+                        //判断通知类型
+                        MediaSessionCompat.Token sessiontoken = NotificationCompat.MediaStyle.getMediaSession(mn.getNotification());
+                        if (sessiontoken != null) {
+                            try {
+                                //获得控制器对象
+                                MediaControllerCompat mediaControllerCompat = new MediaControllerCompat(this,sessiontoken);
+                                mediaControllerCompat.registerCallback(mMediaControllerCallback);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        super.onNotificationPosted(sbn);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void showFloatWindow() {
+        if (Settings.canDrawOverlays(this)) {
+            System.out.println("show");
+            txt.setTextColor(Color.BLACK);
+            windowManager.addView(txt,layoutParams);
+            txt.setOnTouchListener(new FloatingOntouchListener());
+        }
+    }
+
+    private void closeFloatWindow() {
+        if (Settings.canDrawOverlays(this)) {
+            System.out.println("Close");
+            windowManager.removeView(txt);
+        }
+    }
+
+    private class FloatingOntouchListener implements View.OnTouchListener {
+        private int x;
+        private int y;
+        @SuppressLint("ClickableViewAccessibility")
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    x = (int) event.getRawX();
+                    y = (int) event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    int nowX = (int) event.getRawX();
+                    int nowY = (int) event.getRawY();
+                    int movedX = nowX - x;
+                    int movedY = nowY - y;
+                    x = nowX;
+                    y = nowY;
+                    layoutParams.x = layoutParams.x + movedX;
+                    layoutParams.y = layoutParams.y + movedY;
+//                    更新悬浮窗布局
+                    windowManager.updateViewLayout(v, layoutParams);
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+
+    private class receiverlyrics extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            judgelyric = intent.getBooleanExtra("judgelyric",false);
+//            x，y坐标
+            xlabel = intent.getIntExtra("x",0);
+            ylabel = intent.getIntExtra("y",0);
+        }
+    }
+}
